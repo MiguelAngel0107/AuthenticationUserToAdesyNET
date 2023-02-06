968c1668e029b76e4e8a145ec0c69c92762fd595
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-02-05 21:15:47 -0500

    update

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,24 +1,45 @@
 from rest_framework.response import Response
-from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework import status
-
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from django.contrib.auth import get_user_model
 User = get_user_model()
+from apps.user.serializers import UserSerializer
 
 
 
-class RegisterView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        access_code = request.data.get('access_code')
+class RegisterUser(APIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
+    def post(self, request, format=None):
+        data = self.request.data
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        password = data.get('password')
+        access_code = data.get('access_code')
 
-        if not username or not password or not access_code:
+        if not first_name or not password or not access_code or not last_name:
             return Response({'error': 'All fields are required.'},
                             status=status.HTTP_400_BAD_REQUEST)
 
-        user = User.objects.create_user(username=username, password=password, access_code=access_code)
+        user = User.objects.create_user(
+            first_name=first_name, 
+            last_name=last_name,
+            password=password, 
+            access_code=access_code
+            )
         return Response({'status': 'User created successfully'}, status=status.HTTP_201_CREATED)
+
+
+class UserUpdateAPIView(APIView):
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def put(self, request, pk, format=None):
+        user = User.objects.get(pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
